d6e89556ec5d5d16840e00a6562d8a888f725bff
Documenter.jl <documenter@juliadocs.github.io>
2022-12-03 00:05:04 +0000

    build based on 254ad2d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DZIGA","category":"page"},{"location":"#DZIGA","page":"Home","title":"DZIGA","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DZIGA.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DZIGA]","category":"page"}]
+}
